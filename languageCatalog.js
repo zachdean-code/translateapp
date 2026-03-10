a192ff5b60929961cf3c2bd8c489e4b3f0f80217
zachdean-code <zach.dean@gmail.com>
2026-03-10 07:57:05 -0600

    Create languageCatalog.js

---
+++ languageCatalog.js
@@ -0,0 +1,39 @@
+const languages=[
+
+"American English",
+"British English",
+"Australian English",
+
+"Paisa Spanish (Medellín)",
+"Rolo Spanish (Bogotá)",
+"Cali Spanish",
+"Mexican Spanish",
+"Argentine Spanish",
+"Peruvian Spanish",
+
+"Brazilian Portuguese",
+"European Portuguese",
+
+"Modern Standard Arabic",
+"Egyptian Arabic",
+
+"Hindi",
+"Indonesian",
+"Tagalog",
+
+"Swahili",
+"Amharic",
+
+"Farsi",
+"Turkish",
+
+"Chinese",
+"Japanese",
+"Korean",
+"Russian",
+
+"French",
+"German",
+"Italian"
+
+]
